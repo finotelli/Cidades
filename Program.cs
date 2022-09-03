92685883764465037b67f8a67fd460195f8e37d6
Ana Finotelli <finotelli2004@gmail.com>
2022-09-02 22:08:11 -0300

    Cidades

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("São Vicente");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Santos");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Itariri");
+Console.WriteLine("Ilha Comprida");
